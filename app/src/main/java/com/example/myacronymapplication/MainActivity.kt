819e1366d7cc7c9c1f2604d857f11cb0eb1aa421
Jeffrey Joseph <jjrzacal@outlook.com>
2023-02-25 22:16:48 -0800

    minimum requirements - works

--- app/src/main/java/com/example/myacronymapplication/MainActivity.kt
+++ app/src/main/java/com/example/myacronymapplication/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.myacronymapplication
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.view.KeyEvent
 import android.view.LayoutInflater
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.activity.viewModels
@@ -12,35 +15,52 @@ import com.example.myacronymapplication.view.LongFormMainAdapter
 import com.example.myacronymapplication.viewmodel.AcronymsViewModel
 
 class MainActivity : AppCompatActivity(), AcronymsViewModel.ToastCallback {
+    val myViewModel: AcronymsViewModel by viewModels<AcronymsViewModel>()
+    val binding : ActivityMainBinding by lazy  {
+        ActivityMainBinding.inflate(LayoutInflater.from(this))
+    }
+    val inputMethodManager : InputMethodManager by lazy {
+        getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivityMainBinding.inflate(LayoutInflater.from(this))
         val view = binding.root
         setContentView(view)
 
         val myLFAdapter = LongFormMainAdapter()
         binding.resultRecycler.adapter = myLFAdapter
-        val myViewModel: AcronymsViewModel by viewModels<AcronymsViewModel>()
-        myViewModel.setToastCallback(this)
 
-        val inputMethodManager =
-            getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        myViewModel.setToastCallback(this)
 
         myViewModel.longFormList.observeForever() {
             myLFAdapter.setLFList(it)
         }
 
         binding.submitButton.setOnClickListener {
-            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
-            myViewModel.getFullForm(binding.inputText.text.toString())
+            searchResults(binding.inputText.text.toString(), view.windowToken,0)
+        }
+
+        binding.inputText.setOnEditorActionListener  { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                searchResults(binding.inputText.text.toString(), view.windowToken,0)
+                true
+            } else {
+                false
+            }
         }
 
     }
 
     override fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+    }
+
+    fun searchResults(s : String, v : IBinder, f : Int) {
+        binding.inputText.setText(binding.inputText.text.toString().trim())
+        inputMethodManager.hideSoftInputFromWindow(v, f)
+        myViewModel.getFullForm(s)
     }
 
 }
